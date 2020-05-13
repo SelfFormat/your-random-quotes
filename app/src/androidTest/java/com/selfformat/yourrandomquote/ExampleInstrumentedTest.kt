bb2d011b9b25e2f71809c90c7f022c8198ab7fed
Krzysztof <krzysztof@selfformat.com>
2020-05-13 21:22:48 +0200

    add firebase config

--- app/src/androidTest/java/com/selfformat/yourrandomquote/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/selfformat/yourrandomquote/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.yourrandomquote", appContext.packageName)
+        assertEquals("com.selfformat.yourrandomquote", appContext.packageName)
     }
 }
