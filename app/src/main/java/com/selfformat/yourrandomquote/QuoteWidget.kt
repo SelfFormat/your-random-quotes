e5f1de1c5521b401cd80283eb54c0665c053ff7a
Krzysztof <krzysztof@selfformat.com>
2020-05-17 22:02:49 +0200

    add todos for widget layout

--- app/src/main/java/com/selfformat/yourrandomquote/QuoteWidget.kt
+++ app/src/main/java/com/selfformat/yourrandomquote/QuoteWidget.kt
@@ -6,50 +6,38 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
-import android.widget.Toast
-
-private const val TOAST_ACTION = "com.example.android.stackwidget.TOAST_ACTION"
-private const val EXTRA_ITEM = "com.example.android.stackwidget.EXTRA_ITEM"
 
 class QuoteWidget : AppWidgetProvider() {
-
-    override fun onReceive(context: Context?, intent: Intent) {
-        val mgr = AppWidgetManager.getInstance(context)
-        if (intent.action == TOAST_ACTION) {
-            val appWidgetId = intent.getIntExtra(
-                AppWidgetManager.EXTRA_APPWIDGET_ID,
-                AppWidgetManager.INVALID_APPWIDGET_ID
-            )
-            val viewIndex = intent.getIntExtra(EXTRA_ITEM, 0)
-            Toast.makeText(context, "Touched view $viewIndex", Toast.LENGTH_SHORT).show()
-        }
-        super.onReceive(context, intent)
-    }
 
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // Perform this loop procedure for each App Widget that belongs to this provider
-        appWidgetIds.forEach { appWidgetId ->
-            // Create an Intent to launch ExampleActivity
-            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    companion object {
+        private fun constructPendingIntent(context: Context): PendingIntent {
+            return Intent(context, MainActivity::class.java)
                 .let { intent ->
                     PendingIntent.getActivity(context, 0, intent, 0)
                 }
+        }
 
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
+        internal fun updateAppWidget(
+            context: Context, appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
             val views: RemoteViews = RemoteViews(
                 context.packageName,
                 R.layout.quote_widget_layout
             ).apply {
-                setOnClickPendingIntent(R.id.refresh, pendingIntent)
+                setOnClickPendingIntent(R.id.refresh, constructPendingIntent(context))
                 setTextViewText(R.id.quote, "test overriden")
             }
-
-            // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
