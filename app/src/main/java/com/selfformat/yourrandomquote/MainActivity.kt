aa9dc62a77f693214bf4a3b68c7245266379eaf5
Krzysztof <krzysztof@selfformat.com>
2020-05-17 22:27:07 +0200

    add features roadmap

--- app/src/main/java/com/selfformat/yourrandomquote/MainActivity.kt
+++ app/src/main/java/com/selfformat/yourrandomquote/MainActivity.kt
@@ -9,4 +9,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+/*  TODO MVP LIST:
+    [ ] Make navigation graph (login -> quote lists -> Adding quotes)
+    [ ] Add ability to login with firebase (google/email auth)
+    [ ] Fetch user data from firebase
+    [ ] Populate recyclerview with quotes
+    [ ] Display random quote in widget
+    [ ] Add refresh option in widget
+    [ ] Refresh widget on every screen unlock (if possible or something with similar refresh rate)
+    [ ] Polish widget UI
+    [ ] Polish recyclerview UI
+
+    TODO ADDONS:
+    [ ] removing quotes
+    [ ] searching
+*/
+
+
 }
