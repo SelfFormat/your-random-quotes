f200ece1be856e25aff15f11bc3b639aa827bd16
Krzysztof <krzysztof@selfformat.com>
2020-05-24 17:37:53 +0200

    fix setting action bar with navigation component

--- app/src/main/java/com/selfformat/yourrandomquote/MainActivity.kt
+++ app/src/main/java/com/selfformat/yourrandomquote/MainActivity.kt
@@ -4,20 +4,28 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
-    private val navController: NavController
-        get() = findNavController(R.id.nav_host_fragment)
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        setUpActionBar()
     }
 
+    private fun setUpActionBar() {
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
