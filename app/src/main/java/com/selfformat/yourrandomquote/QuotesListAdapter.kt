c3163618aaa2cb593dd26d3f9901ffdfd69a6977
Krzysztof <krzysztof@selfformat.com>
2020-10-04 21:27:43 +0200

    add proper adapter for quotes on main fragment

--- app/src/main/java/com/selfformat/yourrandomquote/QuotesListAdapter.kt
+++ app/src/main/java/com/selfformat/yourrandomquote/QuotesListAdapter.kt
@@ -1,27 +1,28 @@
 package com.selfformat.yourrandomquote
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.quote_list_item.view.*
 
 class QuotesListAdapter(private val quote: List<String?>) :
-    RecyclerView.Adapter<QuotesListAdapter.MyViewHolder>() {
+    RecyclerView.Adapter<QuotesListAdapter.ViewHolder>() {
 
-    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val view = itemView.findViewById<View>(R.id.quoteItemLayout)
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): MyViewHolder {
-        val textView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.quote_list_item, parent, false) as TextView
-        return MyViewHolder(textView)
+        fun bind(string: String?) {
+            view.quoteTextView.text = string
+        }
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.textView.text = quote[position]
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.quote_list_item, parent, false)
+        return ViewHolder(view)
     }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(quote[position])
 
     override fun getItemCount() = quote.size
 }
