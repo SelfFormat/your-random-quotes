20cf48693107adc497d3ca5fddd75894599ffdb2
Krzysztof <krzysztof@selfformat.com>
2020-11-12 18:46:53 +0100

    parcelize quote objects

--- app/src/main/java/com/selfformat/yourrandomquote/domain/DatabaseQuote.kt
+++ app/src/main/java/com/selfformat/yourrandomquote/domain/DatabaseQuote.kt
@@ -1,5 +1,8 @@
 package com.selfformat.yourrandomquote.domain
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 data class DatabaseQuote(
     val quote: String,
     val author: String,
@@ -7,12 +10,13 @@ data class DatabaseQuote(
     val dateAdded: Long = System.currentTimeMillis()
 )
 
+@Parcelize
 data class Quote(
     val quote: String? = null,
     val author: String? = null,
     val category: Category = Category.GENERAL,
     val dateAdded: Long = System.currentTimeMillis()
-) {
+) : Parcelable {
     enum class Category {
         GENERAL,
     }
