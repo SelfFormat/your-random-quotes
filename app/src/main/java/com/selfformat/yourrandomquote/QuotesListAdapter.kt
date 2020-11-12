fa64393f20f7b529e519d40da2575382d88fbd3b
Krzysztof <krzysztof@selfformat.com>
2020-11-12 18:51:25 +0100

    add onclick for picking quote from recyclerview

--- app/src/main/java/com/selfformat/yourrandomquote/QuotesListAdapter.kt
+++ app/src/main/java/com/selfformat/yourrandomquote/QuotesListAdapter.kt
@@ -1,13 +1,15 @@
 package com.selfformat.yourrandomquote
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.selfformat.yourrandomquote.domain.Quote
 import kotlinx.android.synthetic.main.quote_list_item.view.*
 
-class QuotesListAdapter(private val quote: List<Quote?>) :
+class QuotesListAdapter(private val quote: Map<String, Quote>) :
     RecyclerView.Adapter<QuotesListAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -20,11 +22,37 @@ class QuotesListAdapter(private val quote: List<Quote?>) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.quote_list_item, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.quote_list_item,
+                parent,
+                false)
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(quote[position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val quotes = quote.values
+        val ids = quote.keys
+        val singleQuote = quotes.toList()[position]
+        val id = ids.toList()[position]
+        holder.bind(singleQuote)
+        holder.itemView.setOnClickListener(navigateToQuoteDetails(singleQuote, id))
+    }
+
+    private fun navigateToQuoteDetails(quote: Quote?, id: String): View.OnClickListener {
+        return Navigation.createNavigateOnClickListener(
+            R.id.action_mainFragment_to_quoteDetailsFragment,
+            Bundle().apply {
+                putParcelable(QUOTE, quote)
+                putString(QUOTE_ID, id)
+            }
+        )
+    }
 
     override fun getItemCount() = quote.size
+
+    companion object {
+        private const val QUOTE = "key_quote"
+        private const val QUOTE_ID = "key_quote_id"
+    }
 }
