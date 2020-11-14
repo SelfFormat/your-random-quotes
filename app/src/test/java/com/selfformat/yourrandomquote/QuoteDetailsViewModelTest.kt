acb86724d551202940036f34fda411b40ce86baf
Krzysztof <krzysztof@selfformat.com>
2020-11-14 14:08:31 +0100

    add tests for database helper methods for adding updating and removing quotes

--- app/src/test/java/com/selfformat/yourrandomquote/QuoteDetailsViewModelTest.kt
+++ app/src/test/java/com/selfformat/yourrandomquote/QuoteDetailsViewModelTest.kt
@@ -1,16 +1,52 @@
 package com.selfformat.yourrandomquote
 
 import com.google.firebase.database.DatabaseReference
+import com.nhaarman.mockitokotlin2.given
 import com.nhaarman.mockitokotlin2.mock
-import org.junit.Assert.assertEquals
+import com.nhaarman.mockitokotlin2.verify
+import com.selfformat.yourrandomquote.domain.DatabaseQuote
 import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
 
 class QuoteDetailsViewModelTest {
     private val mockDatabaseReference = mock<DatabaseReference>()
     private val viewModel = QuoteDetailsViewModel(mockDatabaseReference)
+    private val mockedDatabaseQuote = mock<DatabaseQuote>()
+
+    private val uid = "userID"
+    private val quoteID = "quoteID"
 
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `verify addQuote will be called with proper values`() {
+        given(mockDatabaseReference.child(anyString())).willReturn(mockDatabaseReference)
+        given(mockDatabaseReference.push()).willReturn(mockDatabaseReference)
+        viewModel.addQuote(uid, mockedDatabaseQuote)
+        verify(mockDatabaseReference).child("users")
+        verify(mockDatabaseReference).child(uid)
+        verify(mockDatabaseReference).child("quotes")
+        verify(mockDatabaseReference).push()
+        verify(mockDatabaseReference).setValue(mockedDatabaseQuote)
+    }
+
+    @Test
+    fun `verify updateQuote will be called with proper values`() {
+        given(mockDatabaseReference.child(anyString())).willReturn(mockDatabaseReference)
+        viewModel.updateQuote(uid, quoteID, mockedDatabaseQuote)
+        verify(mockDatabaseReference).child("users")
+        verify(mockDatabaseReference).child(uid)
+        verify(mockDatabaseReference).child("quotes")
+        verify(mockDatabaseReference).child(quoteID)
+        verify(mockDatabaseReference).setValue(mockedDatabaseQuote)
+    }
+
+    @Test
+    fun `verify removeQuote will be called with proper values`() {
+        given(mockDatabaseReference.child(anyString())).willReturn(mockDatabaseReference)
+        viewModel.removeQuote(uid, quoteID)
+        verify(mockDatabaseReference).child("users")
+        verify(mockDatabaseReference).child(uid)
+        verify(mockDatabaseReference).child("quotes")
+        verify(mockDatabaseReference).child(quoteID)
+        verify(mockDatabaseReference).removeValue()
     }
 }
